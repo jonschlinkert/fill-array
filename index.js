95158952aaf1afed7b9c29f6c04ca06e5e303fa4
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-26 08:58:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*!
+ * fill-array <https://github.com/jonschlinkert/fill-array>
+ *
+ * Copyright (c) 2015 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = fillArray;
+
+function fillArray(arr, num) {
+  if (num === 0) return [];
+
+  var len = arr.length;
+  var max = num - len;
+
+  if (num >= len) {
+    while (max--) {
+      arr[max + len] = arr[max % len];
+    }
+  }
+  return arr;
+}
